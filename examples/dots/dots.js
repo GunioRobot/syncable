ab6f8e462a3e8780d4481f0f93cf34df2ec65780
Pablo Villalba <pablo@teambox.com>
2010-09-27 22:49:05 -0700

    Small fixes

--- examples/dots/dots.js
+++ examples/dots/dots.js
@@ -1,10 +1,11 @@
+const X_OFFSET = 150
+const Y_OFFSET = 250
+
 document.on("dom:loaded", function() {
 
   dots = new Collection("dots")
   dots.template = "<span style='top: {{y}}px; left: {{x}}px'>O</div>"
 
-  const X_OFFSET = 150
-  const Y_OFFSET = 250
 
   new PeriodicalExecuter(function(i){ 
     angle = Date.now() / 1000
